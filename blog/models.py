042a89e5a31478d01201205c9ca728e35fd28df8
alexisreasoner <alexis.reasoner@phbillies.org>
2018-01-25 14:03:38 -0800

    comments

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
@@ -31,3 +31,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+    def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
